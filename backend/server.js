c84cdd30f38e3e697ccefd1091bbe2d3075bb0c4
Abisheksivaraj <abisheksivaraj2001@gmail.com>
2026-04-11 10:43:48 +0530

    add

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,13 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:3000',
+    'https://saravanagraphics.onrender.com',
+    'https://saravana-graphics.onrender.com'
+  ],
   credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
