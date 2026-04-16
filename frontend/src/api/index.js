53f3a0737384fad3a5083419970f1b2dc93ec927
Abisheksivaraj <abisheksivaraj2001@gmail.com>
2026-04-16 19:37:31 +0530

    z

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:5000';
+// export const BASE_URL = 'http://localhost:5000';
 
-// export const BASE_URL = 'https://saravana-graphics.onrender.com';
+export const BASE_URL = 'https://saravana-graphics.onrender.com';
 
 
 const api = axios.create({
